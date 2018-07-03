1839119183592d48eaf50296d2a3aa6e6ef36838
Hasan <hassansajedi@gmail.com>
2018-07-03 18:00:54 +0430

    Update main.py

--- Spam-Filtering/main.py
+++ Spam-Filtering/main.py
@@ -1,6 +1,6 @@
 # Page number 76 > Binary classification without performance matrics
 
-'''python
+'''ruby
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -20,5 +20,4 @@ X_test = vectorizer.transform(X_test_raw)
 
 predictions = classifier.predict(X_test)
 print(predictions)
-
 '''
