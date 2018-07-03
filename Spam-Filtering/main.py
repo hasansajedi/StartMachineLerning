6de4cd5bda6df704b4f85ec7223623059c332515
Hasan <hassansajedi@gmail.com>
2018-07-03 17:38:43 +0430

    Create main.py

---
+++ Spam-Filtering/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+
+df = pd.read_csv('SMSSpamCollection', delimiter='\t',header=None)
+
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(df[1],df[0])
+
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train_raw)
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+
+X_test = vectorizer.transform(X_test_raw)
+
+predictions = classifier.predict(X_test)
+print(predictions)
+
