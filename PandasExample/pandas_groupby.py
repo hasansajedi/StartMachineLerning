6065e206be7329ea193a74fffe90b052362ba295
Hasan <hassansajedi@gmail.com>
2018-09-22 17:05:29 +0330

    Create pandas_groupby.py

---
+++ PandasExample/pandas_groupby.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Create dataframe
+data = {'Company': ['GOOG', 'GOOG', 'MSFT', 'MSFT', 'FB', 'FB'],
+        'Person': ['Sam', 'Charlie', 'Amy', 'Vanessa', 'Carl', 'Sarah'],
+        'Sales': [200, 120, 340, 124, 243, 350]}
+df = pd.DataFrame(data)
+print(df)
+
+byComp = df.groupby('Company')
+print(byComp.mean())
+print(byComp.sum())
+print(byComp.std())
+print(byComp.count())
+print(byComp.max())
+print(byComp.min())
+
+print(byComp.sum().loc['FB'])
+
+print(byComp.describe())
+print(byComp.describe().transpose())
+print(byComp.describe().transpose()['FB'])
